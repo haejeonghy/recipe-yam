1744d93db60524f1079a4a34c8786f4d89008728
hae jeong yu <haejeonghy@gmail.com>
2022-06-09 15:43:20 +0900

    Docs: 테이블 컬럼 수정한 내역 반영하여 create sql 수정

---
+++ docs/yam-create-2022-06-09.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `nickname` varchar(255) UNIQUE NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `posts` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255),
+  `user_id` int NOT NULL,
+  `created_at` datetime DEFAULT (now()),
+  `updated_at` datetime DEFAULT (now()),
+  `recipe` varchar(255),
+  `image_title` varchar(255),
+  `image_path` varchar(255)
+);
+
+CREATE TABLE `ingredients` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `ingredient` varchar(255) NOT NULL
+);
+
+CREATE TABLE `post_ingredients` (
+  `id` int,
+  `post_id` int,
+  `ingredient_id` int
+);
+
+CREATE UNIQUE INDEX `posts_index_0` ON `posts` (`id`);
+
+CREATE UNIQUE INDEX `ingredients_index_1` ON `ingredients` (`id`);
+
+ALTER TABLE `posts` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `post_ingredients` ADD FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+
+ALTER TABLE `post_ingredients` ADD FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`);
